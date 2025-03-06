d284541dd18823e873e294a178e8eebd288ca0fe
two <tung09102002@gmail.com>
2025-03-06 16:30:53 -0700

    add player bullet

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -1,7 +1,26 @@
 extends Area2D
 
-@export var movement_speed : float = 512
-@export var focus_multiplier : float = .6
+const movement_speed : float = 512
+const focus_multiplier : float = .6
+
+const bullet_prefab : PackedScene = preload("res://objects/player/player_bullet.tscn")
+const shoot_cooldown : float = .06
+
+@onready var shoot_timer : float = 0.
+
+func _process(delta: float) -> void:
+    var shoot_normal : bool = Input.is_action_pressed("shoot_rapid")
+    var shoot_focus : bool = Input.is_action_pressed("shoot_focus")
+    if shoot_timer > 0:
+        shoot_timer -= delta
+    elif shoot_normal or shoot_focus:
+        var new_bullet = bullet_prefab.instantiate()
+        new_bullet.position = position + Vector2.UP * 32
+        new_bullet.angle = PI / 2
+        new_bullet.speed = 1024
+        new_bullet.lifetime = 2.
+        get_parent().add_child(new_bullet)
+        shoot_timer = shoot_cooldown
 
 func _physics_process(delta: float) -> void:
     var movement_direction = Input.get_vector(
